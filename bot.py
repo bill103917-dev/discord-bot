8ff8352b09d0713772d671625654403ab6e2869a
bill103917-dev <bill103917@gmail.com>
2026-04-03 11:39:36 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1338,9 +1338,9 @@ async def on_ready():
     cog_list = [
         HelpCog, LogsCog, PingCog, ReactionRoleCog, UtilityCog,
         MinesweeperTextCog, ModerationCog, FunCog, SupportCog,
-         ImageDrawCog, ScheduledUploadCog, BackupSystem, VoiceCog
+         ImageDrawCog, ScheduledUploadCog, BackupSystem
     ]
-#暫時刪除GeminiSystem
+#暫時刪除GeminiSystem, VoiceCog
 
     for cog in cog_list:
         try:
