ac03cbacc8da13f28b978332f254b1f6280c48d6
bill103917-dev <bill103917@gmail.com>
2026-07-06 12:44:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1194,7 +1194,7 @@ async def on_ready():
     cog_list = [
         HelpCog, LogsCog, PingCog, ReactionRoleCog, UtilityCog,
         MinesweeperTextCog, ModerationCog, FunCog, SupportCog,
-        BackupSystem
+        BackupSystem, ImageDrawCog, ScheduledUploadCog
     ]
 
     for cog in cog_list:
